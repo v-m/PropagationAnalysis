3816401bacc62d38f57fffa0ec8d1a8526511e73
Vincenzo Musco <vincenzo.musco@inria.fr>
2015-09-02 01:38:40 +0200

    Adding support for CSV exportation

--- pminer/src/main/java/com/vmusco/pminer/run/MutationStatsRunner.java
+++ pminer/src/main/java/com/vmusco/pminer/run/MutationStatsRunner.java
@@ -43,11 +43,13 @@ public class MutationStatsRunner{
 		options.addOption(opt);
 		opt = new Option("r", "remove-nulls", false, "if set, remove nulls from the medians");
 		options.addOption(opt);
-		opt = new Option("n", "nb-mutants", false, "filter out if more than n mutants are present");
+		opt = new Option("n", "nb-mutants", true, "filter out if more than n mutants are present");
+		options.addOption(opt);
+		opt = new Option("c", "csv", true, "export in csv format with such a separator");
 		options.addOption(opt);
 		opt = new Option("h", "help", false, "display this message");
 		options.addOption(opt);
-
+		
 		CommandLineParser parser = new PosixParser();
 		CommandLine cmd = parser.parse(options, args);
 
@@ -55,6 +57,13 @@ public class MutationStatsRunner{
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp(thisclass.getCanonicalName()+" [options] <usegraph> <mutationFile>", options);
 			System.exit(0);
+		}
+		
+		Character sep = null;
+		
+		if(cmd.hasOption("csv")){
+			if(cmd.getOptionValue("csv").length() == 1)
+				sep = cmd.getOptionValue("csv").charAt(0);
 		}
 
 		HashMap<String, UseGraph> cache = new HashMap<String, UseGraph>();
@@ -90,21 +99,36 @@ public class MutationStatsRunner{
 		GraphML gml = new GraphML(usegraph);
 		gml.load(new FileInputStream(cmd.getArgs()[0]));
 
-		printDataHeader();
+		printDataHeader(sep);
+		
+		final Character ssep = sep;
 		StatisticsDisplayer sd = new StatisticsDisplayer(new MutantTestProcessingListener<StatisticsDisplayer>() {
 			@Override
 			public void aMutantHasBeenProceeded(StatisticsDisplayer a) {
-				System.out.printf("%20s %7d %7d %7d %7d %7d %7.2f %7.2f %7.2f\n",
-						a.getLastMutantId(),
-						(int)a.getSoud().getLastCandidateImpactSetSize(),
-						(int)a.getSoud().getLastActualImpactSetSize(),
-						(int)a.getSoud().getLastIntersectedImpactSetSize(),
-						(int)a.getSoud().getLastFalsePositiveImpactSetSize(),
-						(int)a.getSoud().getLastDiscoveredImpactSetSize(),
-						a.getPrecisionRecallFscore().getLastPrecision(),
-						a.getPrecisionRecallFscore().getLastRecall(),
-						a.getPrecisionRecallFscore().getLastFscore());
-				
+				if(ssep == null){
+					System.out.printf("%20s             %7d %7d %7d %7d %7d %7.2f %7.2f %7.2f\n",
+							a.getLastMutantId(),
+							(int)a.getSoud().getLastCandidateImpactSetSize(),
+							(int)a.getSoud().getLastActualImpactSetSize(),
+							(int)a.getSoud().getLastIntersectedImpactSetSize(),
+							(int)a.getSoud().getLastFalsePositiveImpactSetSize(),
+							(int)a.getSoud().getLastDiscoveredImpactSetSize(),
+							a.getPrecisionRecallFscore().getLastPrecision(),
+							a.getPrecisionRecallFscore().getLastRecall(),
+							a.getPrecisionRecallFscore().getLastFscore());
+				}else{
+					System.out.printf("\"%s\"%c%c%c%d%c%d%c%d%c%d%c%d%c%f%c%f%c%f%c%c%c%c%c\n",
+							a.getLastMutantId(),ssep,ssep,ssep,
+							(int)a.getSoud().getLastCandidateImpactSetSize(),ssep,
+							(int)a.getSoud().getLastActualImpactSetSize(),ssep,
+							(int)a.getSoud().getLastIntersectedImpactSetSize(),ssep,
+							(int)a.getSoud().getLastFalsePositiveImpactSetSize(),ssep,
+							(int)a.getSoud().getLastDiscoveredImpactSetSize(),ssep,
+							a.getPrecisionRecallFscore().getLastPrecision(),ssep,
+							a.getPrecisionRecallFscore().getLastRecall(),ssep,
+							a.getPrecisionRecallFscore().getLastFscore(),ssep,ssep,ssep,ssep,ssep);
+					
+				}
 			}
 		});
 
@@ -144,56 +168,103 @@ public class MutationStatsRunner{
 
 		sd.fireExecutionEnded();
 
-		printLine();
+		printLine(sep);
 		
 		if(cmd.hasOption("remove-nulls")){
 			sd.getPrecisionRecallFscore().removesNulls();
 		}
 		
-		System.out.printf("%20s %7d %7d %7d %7d %7d %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f\n",
-				"MEDIAN",
-				(int)sd.getSoud().getCurrentMedianCandidateImpactSetSize(),
-				(int)sd.getSoud().getCurrentMedianActualImpactSetSize(),
-				(int)sd.getSoud().getCurrentMedianIntersectedImpactSetSize(),
-				(int)sd.getSoud().getCurrentMedianFalsePositiveImpactSetSize(),
-				(int)sd.getSoud().getCurrentMedianDiscoveredImpactSetSize(),
-				sd.getPrecisionRecallFscore().getCurrentMedianPrecision(),
-				sd.getPrecisionRecallFscore().getCurrentMedianRecall(),
-				sd.getPrecisionRecallFscore().getCurrentMedianFscore(),
-				sd.getSoud().getNbSameProportion(),
-				sd.getSoud().getNbSameProportion() + sd.getSoud().getNbOverestimatedProportion(),
-				sd.getSoud().getNbOverestimatedProportion(),
-				sd.getSoud().getNbUnderestimatedProportion(),
-				sd.getSoud().getNbDifferentProportion());
+		if(sep == null){
+			System.out.printf("%20s %5s %5d %7d %7d %7d %7d %7d %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f\n",
+					"MEDIAN",
+					ms.getMutationId(),
+					allMutations.length,
+					(int)sd.getSoud().getCurrentMedianCandidateImpactSetSize(),
+					(int)sd.getSoud().getCurrentMedianActualImpactSetSize(),
+					(int)sd.getSoud().getCurrentMedianIntersectedImpactSetSize(),
+					(int)sd.getSoud().getCurrentMedianFalsePositiveImpactSetSize(),
+					(int)sd.getSoud().getCurrentMedianDiscoveredImpactSetSize(),
+					sd.getPrecisionRecallFscore().getCurrentMedianPrecision(),
+					sd.getPrecisionRecallFscore().getCurrentMedianRecall(),
+					sd.getPrecisionRecallFscore().getCurrentMedianFscore(),
+					sd.getSoud().getNbSameProportion(),
+					sd.getSoud().getNbSameProportion() + sd.getSoud().getNbOverestimatedProportion(),
+					sd.getSoud().getNbOverestimatedProportion(),
+					sd.getSoud().getNbUnderestimatedProportion(),
+					sd.getSoud().getNbDifferentProportion());
+			
+			System.out.printf("%20s %5s %5d %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f\n",
+					"MEAN",
+					ms.getMutationId(),
+					allMutations.length,
+					sd.getSoud().getCurrentMeanCandidateImpactSetSize(),
+					sd.getSoud().getCurrentMeanActualImpactSetSize(),
+					sd.getSoud().getCurrentMeanIntersectedImpactSetSize(),
+					sd.getSoud().getCurrentMeanFalsePositiveImpactSetSize(),
+					sd.getSoud().getCurrentMeanDiscoveredImpactSetSize(),
+					sd.getPrecisionRecallFscore().getCurrentMeanPrecision(),
+					sd.getPrecisionRecallFscore().getCurrentMeanRecall(),
+					sd.getPrecisionRecallFscore().getCurrentMeanFscore(),
+					sd.getSoud().getNbSameProportion(),
+					sd.getSoud().getNbSameProportion() + sd.getSoud().getNbOverestimatedProportion(),
+					sd.getSoud().getNbOverestimatedProportion(),
+					sd.getSoud().getNbUnderestimatedProportion(),
+					sd.getSoud().getNbDifferentProportion());
 		
-		System.out.printf("%20s %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f %7.2f\n",
-				"MEAN",
-				sd.getSoud().getCurrentMeanCandidateImpactSetSize(),
-				sd.getSoud().getCurrentMeanActualImpactSetSize(),
-				sd.getSoud().getCurrentMeanIntersectedImpactSetSize(),
-				sd.getSoud().getCurrentMeanFalsePositiveImpactSetSize(),
-				sd.getSoud().getCurrentMeanDiscoveredImpactSetSize(),
-				sd.getPrecisionRecallFscore().getCurrentMeanPrecision(),
-				sd.getPrecisionRecallFscore().getCurrentMeanRecall(),
-				sd.getPrecisionRecallFscore().getCurrentMeanFscore(),
-				sd.getSoud().getNbSameProportion(),
-				sd.getSoud().getNbSameProportion() + sd.getSoud().getNbOverestimatedProportion(),
-				sd.getSoud().getNbOverestimatedProportion(),
-				sd.getSoud().getNbUnderestimatedProportion(),
-				sd.getSoud().getNbDifferentProportion());
-		
-		printDataHeader();
+			printDataHeader(sep);
+		}else{
+			System.out.printf("\"%s\"%c\"%s\"%c%d%c%d%c%d%c%d%c%d%c%d%c%f%c%f%c%f%c%f%c%f%c%f%c%f%c%f\n",
+					"MEDIAN",sep,
+					ms.getMutationId(),sep,
+					allMutations.length,sep,
+					(int)sd.getSoud().getCurrentMedianCandidateImpactSetSize(),sep,
+					(int)sd.getSoud().getCurrentMedianActualImpactSetSize(),sep,
+					(int)sd.getSoud().getCurrentMedianIntersectedImpactSetSize(),sep,
+					(int)sd.getSoud().getCurrentMedianFalsePositiveImpactSetSize(),sep,
+					(int)sd.getSoud().getCurrentMedianDiscoveredImpactSetSize(),sep,
+					sd.getPrecisionRecallFscore().getCurrentMedianPrecision(),sep,
+					sd.getPrecisionRecallFscore().getCurrentMedianRecall(),sep,
+					sd.getPrecisionRecallFscore().getCurrentMedianFscore(),sep,
+					sd.getSoud().getNbSameProportion(),sep,
+					sd.getSoud().getNbSameProportion() + sd.getSoud().getNbOverestimatedProportion(),sep,
+					sd.getSoud().getNbOverestimatedProportion(),sep,
+					sd.getSoud().getNbUnderestimatedProportion(),sep,
+					sd.getSoud().getNbDifferentProportion());
+			
+			System.out.printf("\"%s\"%c\"%s\"%c%d%c%f%c%f%c%f%c%f%c%f%c%f%c%f%c%f%c%f%c%f%c%f%c%f%c%f\n",
+					"MEAN",sep,
+					ms.getMutationId(),sep,
+					allMutations.length,sep,
+					sd.getSoud().getCurrentMeanCandidateImpactSetSize(),sep,
+					sd.getSoud().getCurrentMeanActualImpactSetSize(),sep,
+					sd.getSoud().getCurrentMeanIntersectedImpactSetSize(),sep,
+					sd.getSoud().getCurrentMeanFalsePositiveImpactSetSize(),sep,
+					sd.getSoud().getCurrentMeanDiscoveredImpactSetSize(),sep,
+					sd.getPrecisionRecallFscore().getCurrentMeanPrecision(),sep,
+					sd.getPrecisionRecallFscore().getCurrentMeanRecall(),sep,
+					sd.getPrecisionRecallFscore().getCurrentMeanFscore(),sep,
+					sd.getSoud().getNbSameProportion(),sep,
+					sd.getSoud().getNbSameProportion() + sd.getSoud().getNbOverestimatedProportion(),sep,
+					sd.getSoud().getNbOverestimatedProportion(),sep,
+					sd.getSoud().getNbUnderestimatedProportion(),sep,
+					sd.getSoud().getNbDifferentProportion());
+		}
 	}
 
-	private static void printDataHeader(){
-		printLine();
-		System.out.println("           Mutant id     CIS     AIS   C^AIS    FPIS     DIS    prec  recall  fscore       S       C       O       U       D");	
-		printLine();
+	private static void printDataHeader(Character sep){
+		printLine(sep);
+		if(sep == null){
+			System.out.printf("           Mutant id    Op  #mut     CIS     AIS   C^AIS    FPIS     DIS    prec  recall  fscore       S       C       O       U       D\n");
+		}else{
+			System.out.printf("MutId%cOp%cnbmut%cCIS%cAIS%cCAIS%cFPIS%cDIS%cprec%crecall%cfscore%cS%cC%cO%cU%cD\n", sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep);
+		}
+		printLine(sep);
 	}
 
-	private static void printLine() {
-		System.out.println("----------------------------------------------------------------------------------------------------------------------------");
-		
+	private static void printLine(Character sep) {
+		if(sep == null){
+			System.out.println("----------------------------------------------------------------------------------------------------------------------------");
+		}
 	}
 
 }
